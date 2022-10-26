266dc7f4620423e4f646df68e563d31082fdc5c6
Stephen Parker <parrotpirate@gmail.com>
2022-10-26 12:51:00 -0400

    wifi off/on aliases

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -1,2 +1,4 @@
 alias updatebrowserlist="npx browserslist@latest --update-db"
 alias browserlistupdate="npx browserslist@latest --update-db"
+alias wifion="networksetup -setairportpower en0 on"
+alias wifioff="networksetup -setairportpower en0 off"
