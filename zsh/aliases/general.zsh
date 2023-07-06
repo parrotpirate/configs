815b812644a9520fabe179ed3cd43289adb2065b
Stephen Parker <parrotpirate@gmail.com>
2023-07-06 09:51:21 -0400

    zsh update alias
    add alias to run update of zsh and plugins

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -1,4 +1,5 @@
 alias wifion="networksetup -setairportpower en0 on"
 alias wifioff="networksetup -setairportpower en0 off"
 alias bk="cd .."
+alias update{omz,zsh}="upgrade_oh_my_zsh_all"
 alias rmcurrent="rmpwd"
