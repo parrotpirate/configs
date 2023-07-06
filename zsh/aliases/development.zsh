cb9eea085b1c38c1d08dcb25c609ebd8090e0b06
Stephen Parker <parrotpirate@gmail.com>
2023-07-06 09:50:08 -0400

    add wp update alias
    add alias to update core, themes, and plugins with wp cli

--- zsh/aliases/development.zsh
+++ zsh/aliases/development.zsh
@@ -1,3 +1,4 @@
 alias fs='finds'
 alias updatebrowserlist="npx browserslist@latest --update-db"
 alias browserlistupdate="npx browserslist@latest --update-db"
+alias wpupdate="wp core update && wp theme update --all && wp plugin update --all"
