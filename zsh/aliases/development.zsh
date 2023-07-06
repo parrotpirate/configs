ad39118c3d751232ac4bfb44a07fce163e1b00b2
Stephen Parker <parrotpirate@gmail.com>
2023-07-06 09:49:29 -0400

    move browser list update alias
    move alias from "general" to "development"

--- zsh/aliases/development.zsh
+++ zsh/aliases/development.zsh
@@ -1,1 +1,3 @@
 alias fs='finds'
+alias updatebrowserlist="npx browserslist@latest --update-db"
+alias browserlistupdate="npx browserslist@latest --update-db"
--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -1,5 +1,3 @@
-alias updatebrowserlist="npx browserslist@latest --update-db"
-alias browserlistupdate="npx browserslist@latest --update-db"
 alias wifion="networksetup -setairportpower en0 on"
 alias wifioff="networksetup -setairportpower en0 off"
 alias bk="cd .."
